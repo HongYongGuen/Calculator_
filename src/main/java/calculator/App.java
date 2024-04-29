a774e7945913fc41f7c9559aece31c18ae6a607c
HongYongGeun <pioneer8919@naver.com>
2024-04-29 14:33:07 +0900

    1-5

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
+        /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
+        /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
+        int[] results = new int[10];
+        int idx=0;
         Scanner sc = new Scanner(System.in);
         while(true){
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -31,6 +35,9 @@ public class App {
                 default -> System.out.println("연산자를 잘못 입력하셨습니다.");
             }
             System.out.println("결과: " + result);
+            /* 연산의 결과를 배열에 저장합니다. */
+            /* index를 증가 시킵니다. */
+            results[idx++]=result;
             sc.nextLine();//enter키 받아주기
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             /* exit을 입력 받으면 반복 종료 */
