634cea2d5bcde4751efc317f12138799e39bd8a8
HongYongGeun <pioneer8919@naver.com>
2024-04-29 15:21:34 +0900

    2-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
         /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
-        List<Integer> results = new ArrayList<>();//리스트로 만듬 , 접근하는 경우가 많은니깐 ArrayList로 만듬
+        Calculator calculator = new Calculator();//Calculatro 객체 생성
         Scanner sc = new Scanner(System.in);
         while(true){
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -20,38 +20,26 @@ public class App {
             // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
             char operation = sc.next().charAt(0);
             int result=0;
-            switch (operation){
-                case '+'-> result = firstNumber + secondNumber;
-                case '-'-> result = firstNumber - secondNumber;
-                case '*'-> result = firstNumber * secondNumber;
-                case '/'-> {
-                    if (secondNumber != 0) {
-                        result = firstNumber / secondNumber;
-                    } else {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        continue;//분모 잘못 입력할 경우 값 저장 안되게 수정
-                    }
-                }
-                default -> {
-                    System.out.println("연산자를 잘못 입력하셨습니다.");
-                    continue;//연산자 잘못 입력할 경우 값 저장 안되게 수정
-                }
+            try {
+                result = calculator.calculate(firstNumber, secondNumber, operation);
+            } catch (BadInputException e) {
+                System.out.println(e.getMessage());
+                continue;
             }
             System.out.println("결과: " + result);
             sc.nextLine();//enter키 받아주기
             /* 연산의 결과를 배열에 저장합니다. */
-            /* index를 증가 시킵니다. */
-            results.add(result);//값 리스트에 추가
+            calculator.results.add(result);//값 리스트에 추가
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             /* 위 요구사항에 맞게 구현 */
             String remove = sc.nextLine();//enter키만 쳐도 넘어가게 nextLine()으로 입력
-            if(remove.toLowerCase().equals("remove")) results.removeFirst();//대소문자 상관없이  입력 확인 첫번째 요소 삭제
+            if(remove.toLowerCase().equals("remove")) calculator.results.removeFirst();//대소문자 상관없이  입력 확인 첫번째 요소 삭제
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             /* 위 요구사항에 맞게 구현 */
             String inquiry = sc.nextLine();//enter키만 쳐도 넘어가게 nextLine()으로 입력
             if(inquiry.toLowerCase().equals("inquiry")){
                 System.out.print("저장된 결과 값 : ");
-                for(int i: results) {
+                for(int i: calculator.results) {
                     System.out.print(i+" ");
                 }
                 System.out.println();
--- src/main/java/calculator/BadInputException.java
+++ src/main/java/calculator/BadInputException.java
@@ -2,6 +2,6 @@ package calculator;
 
 public class BadInputException extends Exception {
     public BadInputException(String type){
-        super("잘못된 입력입니다 ! " + type +"를 입력해주세요");
+        super("잘못된 입력입니다 ! " + type );
     }
 }
