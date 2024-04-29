0548dd74546fd71e6247d613adc36922abf6a9fb
HongYongGeun <pioneer8919@naver.com>
2024-04-29 14:27:18 +0900

    1-3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -16,6 +16,20 @@ public class App {
         System.out.print("사칙연산 기호를 입력하세요: ");
         // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
         char operation = sc.next().charAt(0);
-
+        int result=0;
+        switch (operation){
+            case '+'-> result = firstNumber + secondNumber;
+            case '-'-> result = firstNumber - secondNumber;
+            case '*'-> result = firstNumber * secondNumber;
+            case '/'-> {
+                if (secondNumber != 0) {
+                    result = firstNumber / secondNumber;
+                } else {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                }
+            }
+            default -> System.out.println("연산자를 잘못 입력하셨습니다.");
+        }
+        System.out.println("결과: " + result);
     }
 }
