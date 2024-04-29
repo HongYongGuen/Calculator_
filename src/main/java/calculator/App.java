8f256b7bf1b99ebaf762f501f6d3c8a682b0dbb2
HongYongGeun <pioneer8919@naver.com>
2024-04-29 14:47:33 +0900

    1-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,14 +1,13 @@
 package calculator;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
 
     public static void main(String[] args) {
         /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
         /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
-        int[] results = new int[10];
-        int idx=0;
+        List<Integer> results = new ArrayList<>();//리스트로 만듬 , 접근하는 경우가 많은니깐 ArrayList로 만듬
         Scanner sc = new Scanner(System.in);
         while(true){
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -39,16 +38,15 @@ public class App {
                 }
             }
             System.out.println("결과: " + result);
+            sc.nextLine();//enter키 받아주기
             /* 연산의 결과를 배열에 저장합니다. */
             /* index를 증가 시킵니다. */
-            if(idx<9) results[idx++]=result;
-            else{
-                for(int i=0;i<8;i++){
-                    results[i]=results[i+1];
-                }
-                results[idx]=result;
-            }
-            sc.nextLine();//enter키 받아주기
+            results.add(result);//값 리스트에 추가
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            /* 위 요구사항에 맞게 구현 */
+            String remove = sc.nextLine();//enter키만 쳐도 넘어가게 nextLine()으로 입력
+            if(remove.toLowerCase().equals("remove")) results.removeFirst();//대소문자 상관없이  입력 확인
+            //for(int i=0;i<results.size();i++) System.out.println(results.get(i));
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             /* exit을 입력 받으면 반복 종료 */
             String exit = sc.nextLine();//enter키만 쳐도 넘어가게 nextLine()으로 입력
